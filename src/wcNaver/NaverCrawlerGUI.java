8df3f5b11cb9bdf1b8664b5f6d5883eaec1f861e
TurtleShip <sk765@cornell.edu>
2014-01-28 15:26:56 +0900

    Implemented place holder for JPanels

--- src/wcNaver/NaverCrawlerGUI.java
+++ src/wcNaver/NaverCrawlerGUI.java
@@ -13,15 +13,18 @@ import java.awt.*;
  */
 public class NaverCrawlerGUI implements NaverConstants {
 
-    private JPanel selection;
+    private JPanel myFacePanel; // TODO: When I am ready, post my handsome face.
+    private JPanel selectionPanel;
+    private JPanel webtoonListPanel;
+    private JPanel progressPanel;
 
     private JLabel mainLabel;
     private JLabel subLabel;
 
-    private JList mainSelector;
-    private JList subSelector;
+    private JList mainSelectorList;
+    private JList subSelectorList;
 
-    private JButton jbtnGetList;
+    private JButton jbtnGetListBtn;
 
     public NaverCrawlerGUI() {
 
@@ -33,49 +36,62 @@ public class NaverCrawlerGUI implements NaverConstants {
         }
 
         JFrame jfrm = new JFrame("네이버 크롤러");
-        jfrm.setLayout(new FlowLayout());
-        jfrm.setSize(600, 400);
+        jfrm.setLayout(new FlowLayout(FlowLayout.CENTER, 2, 2));
+        jfrm.setSize(625, 720);
+        jfrm.setResizable(false); // Make it not resizable for now.
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         // Create a selection panel where users will select cartoons
-        selection = new JPanel();
-        selection.setLayout(new FlowLayout(FlowLayout.CENTER, 2, 2));
-        selection.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
-        selection.setPreferredSize(new Dimension(310, 180));
+        selectionPanel = new JPanel();
+        selectionPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 2, 2));
+        selectionPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        selectionPanel.setPreferredSize(new Dimension(310, 180));
 
         // Create the main selector with a label
         mainLabel = new JLabel("메인 카테고리");
         mainLabel.setPreferredSize(new Dimension(150, 10));
-        mainSelector = new JList(MAIN_CAT);
-        mainSelector.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        mainSelector.setSelectedIndex(0);
-        mainSelector.setPreferredSize(new Dimension(150, 130));
-        mainSelector.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
+        mainSelectorList = new JList(MAIN_CAT);
+        mainSelectorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        mainSelectorList.setSelectedIndex(0);
+        mainSelectorList.setPreferredSize(new Dimension(150, 130));
+        mainSelectorList.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
 
         // Create the sub selector with a label
         subLabel = new JLabel("하위 카테고리");
         subLabel.setPreferredSize(new Dimension(150, 10));
-        subSelector = new JList(WEBTOON_CAT);
-        subSelector.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        subSelector.setPreferredSize(new Dimension(150, 130));
-        subSelector.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
+        subSelectorList = new JList(WEBTOON_CAT);
+        subSelectorList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        subSelectorList.setPreferredSize(new Dimension(150, 130));
+        subSelectorList.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
 
         // Add a button that user can click to download stuff
-        jbtnGetList = new JButton("웹툰 목록 가져오기~");
-        jbtnGetList.setPreferredSize(new Dimension(150, 20));
+        jbtnGetListBtn = new JButton("웹툰 목록 가져오기~");
+        jbtnGetListBtn.setPreferredSize(new Dimension(150, 20));
 
-        // add them all to the selection panel
-        selection.add(mainLabel);
-        selection.add(subLabel);
-        selection.add(mainSelector);
-        selection.add(subSelector);
-        selection.add(jbtnGetList);
+        // add them all to the selectionPanel panel
+        selectionPanel.add(mainLabel);
+        selectionPanel.add(subLabel);
+        selectionPanel.add(mainSelectorList);
+        selectionPanel.add(subSelectorList);
+        selectionPanel.add(jbtnGetListBtn);
 
 
 
-        // Add the selection to the main frame
-        jfrm.add(selection);
+        // Create a progress panel
+        progressPanel = new JPanel();
+        progressPanel.setPreferredSize(new Dimension(310, 180));
+        progressPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        // Create a webtoon list panel
+        webtoonListPanel = new JPanel();
+        webtoonListPanel.setPreferredSize(new Dimension(620, 500));
+        webtoonListPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+
+        // Add the selectionPanel to the main frame
+        jfrm.add(selectionPanel);
+        jfrm.add(progressPanel);
+        jfrm.add(webtoonListPanel);
 
         jfrm.setVisible(true);
     }
